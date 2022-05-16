c86aa2b370ddb7c0371c045364ba406f7742265f
pikachu04230 <leo0009859@gmail.com>
2022-05-16 23:12:43 +0800

    Test Simple Discounts

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -21,4 +21,15 @@ describe('Potter', () => {
       potter.buy([1, 1, 1]);
       expect(potter.price).toBe(8 * 3);
     });
+
+    test('Test Simple Discounts', () => {
+        potter.buy([0, 1]);
+        expect(potter.price).toBe(8 * 2 * 0.95);
+        potter.buy([0, 2, 4]);
+        expect(potter.price).toBe(8 * 3 * 0.9);
+        potter.buy([0, 1, 2, 4]);
+        expect(potter.price).toBe(8 * 4 * 0.8);
+        potter.buy([0, 1, 2, 3, 4]);
+        expect(potter.price).toBe(8 * 5 * 0.75);
+      });
   });
--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -9,7 +9,27 @@ export class Potter {
             return 0;
         }
         else {
-            return this.books.length * 8;
+            switch (this.numTitles()) {
+                case 1:
+                    return this.books.length * 8;
+                case 2:
+                    return this.books.length * 8 * 0.95;
+                case 3:
+                    return this.books.length * 8 * 0.9;
+                case 4:
+                    return this.books.length * 8 * 0.8;
+                default:    // numTitles == 5
+                    return this.books.length * 8 * 0.75;
+            }
         }
+    }
+
+    private numTitles() {
+        let numTitles = 0;
+        for (let i = 0; i < 5; i++) {
+            if (this.books.includes(i))
+                numTitles++;
+        }
+        return numTitles;
     }
 }
